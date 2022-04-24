851f9c24a4e1136c60b5da6f904bcc28223932e8
KostyaTsymbal <kostiantyn.tsymbal@yalantis.net>
2022-04-24 18:32:21 +0300

    Added config

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct Config {
+    pub root_dir: String,
+    pub port: i32,
+}
+
+impl Config {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let port = match args.next() {
+            Some(arg) => match arg.parse::<i32>() {
+                Ok(val) => val,
+                Err(_) => return Err("Unable to parse port")
+            },
+            None => return Err("Didn't get port"),
+        };
+
+        let root_dir = match args.next() {
+            Some(arg) => arg,
+            None => "/home".to_string(),
+        };
+
+        Ok(Config {
+            port,
+            root_dir
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::process;
+
+use test_server::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Starting test server...");
+    println!("{:?}", config)
 }
